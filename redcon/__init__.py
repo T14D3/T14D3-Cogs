8c588d3dd4be417b5b65fceaaafc19e031ccd0f8
T14D3 <sundmacher.tiade@gmail.com>
2023-08-24 01:49:23 +0200

    Update __init__.py

--- redcon/__init__.py
+++ redcon/__init__.py
@@ -2,8 +2,3 @@ from .rcon import RedCon
 
 async def setup(bot):
     await bot.add_cog(RedCon(bot))
-
-    bot.tree.add_command(run_rcon)
-
-async def teardown(bot):
-    bot.tree.remove_command(run_rcon)
