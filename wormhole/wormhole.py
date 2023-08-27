385d541c5bb6d4401d4f2410128a4ddc547df4b2
T14D3 <sundmacher.tiade@gmail.com>
2023-08-27 05:57:09 +0200

    Automatic shit? Maybe?

--- wormhole/wormhole.py
+++ wormhole/wormhole.py
@@ -5,7 +5,17 @@ class WormHole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier="wormhole", force_registration=True)
+        self.bot.loop.create_task(self.setup_listeners())
         
+    async def setup_listeners(self):
+        await self.bot.wait_until_ready()
+        source_channels = await self.config.all()
+        for source_id, destination_id in source_channels["linked_channels"].items():
+            source_channel = self.bot.get_channel(int(source_id))
+            if source_channel:
+                source_channel._relay_destination_id = int(destination_id)
+                self.bot.add_listener(self.on_source_message, "on_message", check=lambda message: message.channel == source_channel)
+    
     @commands.command()
     async def link(self, ctx, destination_channel_id: int):
         """Link the current channel to a destination channel for sending messages."""
@@ -15,30 +25,17 @@ class WormHole(commands.Cog):
         
         await ctx.send(f"This channel is now linked to the destination channel with ID {destination_channel_id}.")
     
-    @commands.command()
-    async def sendmessage(self, ctx, *, message: str):
-        """Send a message to the linked destination channel using a webhook."""
-        source_channel_id = ctx.channel.id
-        
-        destination_channel_id = await self.config.get_raw(f"linked_channels.{source_channel_id}")
-        if not destination_channel_id:
-            await ctx.send("This channel is not linked to a destination channel.")
-            return
-        
-        destination_channel = self.bot.get_channel(destination_channel_id)
-        
-        if not destination_channel:
-            await ctx.send("The linked destination channel does not exist.")
-            return
-        
-        webhook = await self.find_bot_webhook(destination_channel)
-        
-        if not webhook:
-            # Create a new webhook with the user's profile picture and name
-            webhook = await destination_channel.create_webhook(name=ctx.author.display_name)
-        
-        await webhook.send(message, username=ctx.author.display_name, avatar_url=str(ctx.author.avatar.url) if ctx.author.avatar else None)
-        await ctx.send("Message sent to the linked destination channel using your profile information.")
+    async def on_source_message(self, message):
+        if not message.author.bot:
+            destination_channel_id = getattr(message.channel, "_relay_destination_id", None)
+            if destination_channel_id:
+                destination_channel = self.bot.get_channel(destination_channel_id)
+                if destination_channel:
+                    webhook = await self.find_bot_webhook(destination_channel)
+                    if not webhook:
+                        # Create a new webhook with the user's profile picture and name
+                        webhook = await destination_channel.create_webhook(name=message.author.display_name)
+                    await webhook.send(message.content, username=message.author.display_name, avatar_url=str(message.author.avatar.url) if message.author.avatar else None)
     
     async def find_bot_webhook(self, channel):
         webhooks = await channel.webhooks()
