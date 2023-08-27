20f82401d1658c01949f4335d6e20f348767751c
T14D3 <sundmacher.tiade@gmail.com>
2023-08-27 16:59:04 +0200

    Update wormhole.py

--- wormhole/wormhole.py
+++ wormhole/wormhole.py
@@ -5,7 +5,7 @@ class WormHole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier="wormhole", force_registration=True)
-        self.config.register_global(linked_channels=[])  # Initialize the configuration
+        self.config.register_global(linked_channels_list=[])  # Initialize the configuration
         
         self.bot.loop.create_task(self.setup_listeners())
         
@@ -16,10 +16,10 @@ class WormHole(commands.Cog):
     @commands.command()
     async def link(self, ctx):
         """Link the current channel to the network."""
-        linked_channels = await self.config.linked_channels()
+        linked_channels = await self.config.linked_channels_list()
         if ctx.channel.id not in linked_channels:
             linked_channels.append(ctx.channel.id)
-            await self.config.linked_channels.set(linked_channels)
+            await self.config.linked_channels_list.set(linked_channels)
             await ctx.send("This channel is now linked to the network.")
         else:
             await ctx.send("This channel is already linked.")
@@ -27,10 +27,10 @@ class WormHole(commands.Cog):
     @commands.command()
     async def unlink(self, ctx):
         """Unlink the current channel from the network."""
-        linked_channels = await self.config.linked_channels()
+        linked_channels = await self.config.linked_channels_list()
         if ctx.channel.id in linked_channels:
             linked_channels.remove(ctx.channel.id)
-            await self.config.linked_channels.set(linked_channels)
+            await self.config.linked_channels_list.set(linked_channels)
             await ctx.send("This channel is no longer linked to the network.")
         else:
             await ctx.send("This channel is not linked.")
@@ -42,7 +42,7 @@ class WormHole(commands.Cog):
         if not message.channel.permissions_for(message.guild.me).send_messages:
             return
         
-        linked_channels = await self.config.linked_channels()
+        linked_channels = await self.config.linked_channels_list()
         if message.channel.id in linked_channels:
             for channel_id in linked_channels:
                 if channel_id != message.channel.id:
