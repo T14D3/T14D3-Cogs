a6fc4cfd479d99e4750012dda23cfe04b08679bf
T14D3 <sundmacher.tiade@gmail.com>
2023-08-27 05:35:35 +0200

    Webhook questionmark

--- wormhole/wormhole.py
+++ wormhole/wormhole.py
@@ -7,15 +7,20 @@ class WormHole(commands.Cog):
         
     @commands.command()
     async def sendmessage(self, ctx, channel_id: int, *, message: str):
-        """Send a message to a specific channel."""
+        """Send a message to a specific channel using a webhook."""
         channel = self.bot.get_channel(channel_id)
         
         if not channel:
             await ctx.send("Invalid channel ID provided.")
             return
         
-        await channel.send(message)
-        await ctx.send(f"Message sent to {channel.mention}.")
+        # Create a webhook with the user's profile picture and name
+        webhook = await channel.create_webhook(name=ctx.author.display_name, avatar=ctx.author.avatar_url)
+        
+        # Send the message using the webhook
+        await webhook.send(message, username=ctx.author.display_name, avatar_url=ctx.author.avatar_url)
+        
+        await ctx.send(f"Message sent to {channel.mention} using your profile information.")
     
 def setup(bot):
     bot.add_cog(WormHole(bot))
