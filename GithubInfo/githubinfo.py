fc6872e56242b89407ce10981194f6817736ceee
T14D3 <sundmacher.tiade@gmail.com>
2023-09-01 15:12:09 +0200

    Update githubinfo.py

--- GithubInfo/githubinfo.py
+++ GithubInfo/githubinfo.py
@@ -8,9 +8,10 @@ class GithubInfo(commands.Cog):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)  # Change the identifier to a unique integer
         self.config.register_global(github_token=None, text_template="GitHub Stars: {}")
+        self.config.register_guild(channels={})
         self.task = None
 
-    async def update_voice_channel(self):
+    async def update_voice_channels(self):
         await self.bot.wait_until_ready()
         while not self.bot.is_closed():
             github_keys = await self.bot.get_shared_api_tokens("github")
@@ -20,50 +21,119 @@ class GithubInfo(commands.Cog):
                 # Create a GitHub instance
                 github = Github(github_token)
 
-                try:
-                    # Get the repository (replace owner and repo_name with your values)
-                    repo = github.get_repo("owner/repo_name")
+                for guild_id, channel_info in (await self.config.all_guilds()).items():
+                    guild = self.bot.get_guild(guild_id)
+                    if guild is None:
+                        continue
 
-                    # Get the number of stars
-                    stars = repo.stargazers_count
+                    for channel_id, data in channel_info.get("channels", {}).items():
+                        try:
+                            # Get the repository
+                            repo = github.get_repo(data["repo_link"])
 
-                    # Get the configured text template
-                    text_template = await self.config.text_template()
+                            # Get the number of stars
+                            stars = repo.stargazers_count
 
-                    # Update the voice channel names in all guilds
-                    for guild in self.bot.guilds:
-                        for channel in guild.voice_channels:
-                            if channel.id == int(channel_id):
-                                new_name = text_template.format(stars)
+                            # Get the configured text template
+                            text_template = data.get("text_template", await self.config.text_template())
+
+                            # Update the voice channel name
+                            channel = guild.get_channel(channel_id)
+                            if channel:
+                                new_name = text_template.format(count=stars)
                                 await channel.edit(name=new_name)
-                except Exception as e:
-                    print(f"An error occurred: {str(e)}")
+                        except Exception as e:
+                            print(f"An error occurred: {str(e)}")
 
             await asyncio.sleep(300)  # Sleep for 5 minutes
 
     @commands.command()
-    async def settexttemplate(self, ctx, *, template: str):
-        """Set the text template for the voice channel name."""
-        await self.config.text_template.set(template)
-        await ctx.send("Text template set successfully.")
+    async def githubinfo(self, ctx, *args):
+        """Main command for GitHub info."""
+        if not args:
+            return await ctx.send("Usage: `[p]githubinfo stars <Repo link>` or `[p]githubinfo channel <Channel-ID> <Repo Link> <Message>`")
 
-    @commands.command()
-    async def startgithubstars(self, ctx, channel_id: int):
-        """Start updating a voice channel with GitHub stars count."""
-        if self.task and not self.task.done():
-            return await ctx.send("The GitHub stars update task is already running.")
-
-        self.task = self.bot.loop.create_task(self.update_voice_channel())
-        await ctx.send(f"GitHub stars updates started for Voice Channel ID {channel_id}.")
-
-    @commands.command()
-    async def stopgithubstars(self, ctx):
-        """Stop updating the voice channel with GitHub stars count."""
-        if self.task and not self.task.done():
-            self.task.cancel()
-            await ctx.send("GitHub stars updates stopped.")
+        subcommand = args[0].lower()
+        if subcommand == "stars":
+            await self.github_stars(ctx, args[1])
+        elif subcommand == "channel":
+            await self.add_channel(ctx, args[1:])
+        elif subcommand == "removechannel":
+            await self.remove_channel(ctx, args[1])
         else:
-            await ctx.send("No GitHub stars update task is currently running.")
+            await ctx.send("Invalid subcommand. Usage: `[p]githubinfo stars <Repo link>` or `[p]githubinfo channel <Channel-ID> <Repo Link> <Message>`")
+
+    async def github_stars(self, ctx, repo_link):
+        """Manually query GitHub stars."""
+        github_keys = await self.bot.get_shared_api_tokens("github")
+        github_token = github_keys.get("api_key")
+
+        if github_token is None:
+            return await ctx.send("The GitHub API token has not been set. Please ask a bot administrator to set it using `[p]set api github api_key,TOKEN`.")
+
+        try:
+            # Create a GitHub instance
+            github = Github(github_token)
+
+            # Get the repository
+            repo = github.get_repo(repo_link)
+
+            # Get the number of stars
+            stars = repo.stargazers_count
+
+            await ctx.send(f"Stars for {repo_link}: {stars}")
+        except Exception as e:
+            await ctx.send(f"An error occurred: {str(e)}")
+
+    async def add_channel(self, ctx, args):
+        """Add a channel for GitHub stars updates."""
+        if len(args) < 3:
+            return await ctx.send("Usage: `[p]githubinfo channel <Channel-ID> <Repo Link> <Message>`")
+
+        channel_id, repo_link, message = args[0], args[1], " ".join(args[2:])
+
+        try:
+            channel_id = int(channel_id)
+        except ValueError:
+            return await ctx.send("Invalid Channel-ID. Please provide a valid numeric Channel-ID.")
+
+        github_keys = await self.bot.get_shared_api_tokens("github")
+        github_token = github_keys.get("api_key")
+
+        if github_token is None:
+            return await ctx.send("The GitHub API token has not been set. Please ask a bot administrator to set it using `[p]set api github api_key,TOKEN`.")
+
+        try:
+            # Create a GitHub instance
+            github = Github(github_token)
+
+            # Check if the repository exists
+            github.get_repo(repo_link)
+
+            # Save the channel info in guild configuration
+            async with self.config.guild(ctx.guild).channels() as channels:
+                channels[channel_id] = {"repo_link": repo_link, "text_template": message}
+            await ctx.send(f"Channel with ID {channel_id} added for GitHub stars updates.")
+        except Exception as e:
+            await ctx.send(f"An error occurred: {str(e)}")
+
+    async def remove_channel(self, ctx, channel_id):
+        """Remove a channel from GitHub stars updates."""
+        try:
+            channel_id = int(channel_id)
+        except ValueError:
+            return await ctx.send("Invalid Channel-ID. Please provide a valid numeric Channel-ID.")
+
+        async with self.config.guild(ctx.guild).channels() as channels:
+            if channel_id in channels:
+                del channels[channel_id]
+                await ctx.send(f"Channel with ID {channel_id} removed from GitHub stars updates.")
+            else:
+                await ctx.send("Channel not found in the configuration.")
+
+    def cog_unload(self):
+        if self.task:
+            self.task.cancel()
 
 def setup(bot):
     bot.add_cog(GithubInfo(bot))
